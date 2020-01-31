73d0c626e43584c1ace73d0e610ec07c50ebbfb3
Narwhal.Digital <allison@narwhal.digital>
2020-01-31 16:52:07 +0000

    Bitbucket Pipelines automated deployment.

---
+++ wp-content/themes/reliant/functions/setup.php
@@ -0,0 +1,20 @@
+<?php
+/****
+ * 
+ * Theme Setup
+ */
+
+if (! function_exists('reliant_setup') ) : 
+function reliant_setup() {
+	register_nav_menus( array(
+		'header' => esc_html__( 'Header', 'reliant' ),
+		'footer-menu-primary' => esc_html__('Landing Page Footer', 'reliant'),
+		'footer-menu-secondary' => esc_html__('Footer Menu Secondary', 'reliant')
+	) );
+}
+endif;
+add_action('after_setup_theme', 'reliant_setup');
+
+if( function_exists('acf_add_options_page') ) {
+	acf_add_options_page();
+}
