1caaed69a00be5dd68bd7230ba2df288810fbf40
Yancey Vickers <yancey.vickers@narwhal.digital>
2020-04-17 17:45:30 -0400

    Gravity form options for certain select inputs

--- wp-content/themes/reliant/functions/setup.php
+++ wp-content/themes/reliant/functions/setup.php
@@ -1,10 +1,10 @@
 <?php
 /****
- * 
+ *
  * Theme Setup
  */
 
-if (! function_exists('reliant_setup') ) : 
+if (! function_exists('reliant_setup') ) :
 function reliant_setup() {
 	register_nav_menus( array(
 		'header' => esc_html__( 'Header', 'reliant' ),
@@ -26,3 +26,22 @@ if( function_exists('acf_add_options_page') ) {
 		)
 	);
 }
+
+function gravity_form_choices( $field ){
+	if( !class_exists( 'GFAPI' ) ){
+		return $field;
+	}
+
+	$forms = GFAPI::get_forms( true, false );
+	$field['choices'] = array();
+
+	foreach( $forms as $form ){
+		$field['choices'][$form['id']] = $form['title'];
+	}
+
+	return $field;
+}
+
+add_filter( 'acf/load_field/name=form_id', 'gravity_form_choices' );
+add_filter( 'acf/load_field/name=tab_1_form_id', 'gravity_form_choices' );
+add_filter( 'acf/load_field/name=tab_2_form_id', 'gravity_form_choices' );
